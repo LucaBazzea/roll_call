0b7bf2849885c9aeb92ca70a7f20b653e28c75f7
Luca <auth@luca.cx>
2025-02-19 20:23:41 +0200

    /user/

--- app/api.py
+++ app/api.py
@@ -4,8 +4,35 @@ from django.core.cache import cache
 from ninja import NinjaAPI
 from ninja.responses import Response
 
-from .models import User, Connection
 from app import models, schema, services
 
 
 api = NinjaAPI()
+
+
+# TODO: Return gym info
+@api.get("/user/")
+def get_user(request, id: int):
+    try:
+        user = models.User.objects.get(id=id)
+    except models.User.DoesNotExist:
+        return 404
+
+    try:
+        belt_bjj_query = models.BeltBJJ.objects.get(user=user)
+
+        belt_bjj = {
+            "colour": belt_bjj_query.belt_colour,
+            "stripes": belt_bjj_query.stripes
+        }
+    except:
+        belt_bjj = None
+
+    response = {
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
+        "belt_bjj": belt_bjj
+    }
+
+    return response
