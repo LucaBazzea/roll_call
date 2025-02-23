a79a24012fbcb94fbd0ea7a1b9e1b70326b925ed
Luca <auth@luca.cx>
2025-02-23 20:41:28 +0200

    Router stuff

---
+++ client/src/routes/gym/schedule/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex flex-row border-2 border-red-600">
+	<div id="mon">Mon</div>
+	<div id="tue">Tue</div>
+	<div id="wed">Wed</div>
+	<div id="thu">Thu</div>
+	<div id="fri">Fri</div>
+	<div id="sat">Sat</div>
+	<div id="sun">Sun</div>
+</div>
