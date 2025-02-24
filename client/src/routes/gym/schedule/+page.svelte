f926df5e3c37bf00ea3babb08c9e6de596bb5ee3
Luca <auth@luca.cx>
2025-02-24 19:30:05 +0200

    Calendar WIP

--- client/src/routes/gym/schedule/+page.svelte
+++ client/src/routes/gym/schedule/+page.svelte
@@ -1,9 +1,107 @@
-<div class="flex flex-row border-2 border-red-600">
-	<div id="mon">Mon</div>
-	<div id="tue">Tue</div>
-	<div id="wed">Wed</div>
-	<div id="thu">Thu</div>
-	<div id="fri">Fri</div>
-	<div id="sat">Sat</div>
-	<div id="sun">Sun</div>
+<div class="flex h-max flex-row border-2 border-red-600 text-center">
+	<div id="mon" class="flex w-1/6 flex-col">
+		<h1>Mon</h1>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+	</div>
+	<div id="tue" class="flex w-1/6 flex-col">
+		<div>Tue</div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+	</div>
+	<div id="wed" class="flex w-1/6 flex-col">
+		<div>Wed</div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+	</div>
+	<div id="thu" class="flex w-1/6 flex-col">
+		<div>Thu</div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+	</div>
+	<div id="fri" class="flex w-1/6 flex-col">
+		<div>Fri</div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+	</div>
+	<div id="sat" class="flex w-1/6 flex-col">
+		<div>Sat</div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+	</div>
+	<div id="sun" class="flex w-1/6 flex-col">
+		<div>Sun</div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+		<div class="size-full border-2 border-gray-500"></div>
+	</div>
 </div>
