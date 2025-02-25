c018fa10ddbc394b6142c9b69f8cb38bb9438451
Luca <auth@luca.cx>
2025-02-25 20:51:01 +0200

    h-screen and time column WIP

--- client/src/routes/gym/schedule/+page.svelte
+++ client/src/routes/gym/schedule/+page.svelte
@@ -1,4 +1,19 @@
-<div class="flex h-max flex-row border-2 border-red-600 text-center">
+<div class="flex h-screen flex-row text-center">
+	<div class="flex w-12 flex-col border-r-2 border-gray-500">
+		<div class="h-[300px]"></div>
+		<div class="size-full border-2 border-gray-500">1</div>
+		<div class="size-full border-2 border-gray-500">2</div>
+		<div class="size-full border-2 border-gray-500">3</div>
+		<div class="size-full border-2 border-gray-500">4</div>
+		<div class="size-full border-2 border-gray-500">5</div>
+		<div class="size-full border-2 border-gray-500">6</div>
+		<div class="size-full border-2 border-gray-500">7</div>
+		<div class="size-full border-2 border-gray-500">8</div>
+		<div class="size-full border-2 border-gray-500">9</div>
+		<div class="size-full border-2 border-gray-500">10</div>
+		<div class="size-full border-2 border-gray-500">11</div>
+		<div class="size-full border-2 border-gray-500">12</div>
+	</div>
 	<div id="mon" class="flex w-1/6 flex-col">
 		<h1>Mon</h1>
 		<div class="size-full border-2 border-gray-500"></div>
