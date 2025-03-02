1f0ceee20cb2de99032adeebcecd5a3b5594de48
Luca <auth@luca.cx>
2025-03-02 22:37:15 +0200

    Add title to event

--- client/src/routes/gym/schedule/+page.svelte
+++ client/src/routes/gym/schedule/+page.svelte
@@ -1,14 +1,14 @@
 <script>
 	let schedule = {
 		mon: [
-			{ start: '11:00', end: '12:30' },
-			{ start: '18:00', end: '19:30' }
+			{ title: 'No-gi', start: '11:00', end: '12:30' },
+			{ title: 'No-gi', start: '18:00', end: '19:30' }
 		],
 		tue: [],
 		wed: [],
 		thu: [
-			{ start: '16:00', end: '17:30' },
-			{ start: '18:00', end: '19:30' }
+			{ title: 'No-gi', start: '16:00', end: '17:30' },
+			{ title: 'No-gi', start: '18:00', end: '19:30' }
 		],
 		fri: [],
 		sat: [],
@@ -28,25 +28,25 @@
 	}
 </script>
 
-<div class="grid grid-cols-8 gap-4 text-sm">
-	<div class="col-span-1 font-bold">Time</div>
-	<div class="col-span-1 font-bold">Monday</div>
-	<div class="col-span-1 font-bold">Tuesday</div>
-	<div class="col-span-1 font-bold">Wednesday</div>
-	<div class="col-span-1 font-bold">Thursday</div>
-	<div class="col-span-1 font-bold">Friday</div>
-	<div class="col-span-1 font-bold">Saturday</div>
-	<div class="col-span-1 font-bold">Sunday</div>
+<div class="grid grid-cols-8 p-4 text-sm">
+	<div class="col-span-1 border-b-2 border-gray-300 font-bold">Time</div>
+	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Monday</div>
+	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Tuesday</div>
+	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Wednesday</div>
+	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Thursday</div>
+	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Friday</div>
+	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Saturday</div>
+	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Sunday</div>
 
 	{#each Array.from({ length: 24 }, (_, i) => i) as hour}
 		<div class="col-span-1 font-bold">{formatTime(`${hour}:00`)}</div>
 		{#each Object.keys(schedule) as day}
-			<div class="col-span-1">
+			<div class="col-span-1 border-1 border-gray-300">
 				{#each schedule[day] as event}
 					{#if event.start.split(':')[0] === `${hour}`}
-						<div class="mb-2 rounded-md bg-blue-200 p-2">
+						<div class="mb-2 bg-blue-200 p-2">
+							<div class="font-bold">{event.title}</div>
 							<div class="font-bold">{formatTime(event.start)} - {formatTime(event.end)}</div>
-							<div>{getDuration(event.start, event.end)}</div>
 						</div>
 					{/if}
 				{/each}
