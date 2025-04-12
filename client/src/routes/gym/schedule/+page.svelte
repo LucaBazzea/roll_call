2dd057df64a2d39264fb50f3affd3eb288468b4f
Luca <auth@luca.cx>
2025-04-12 11:22:08 +0200

    Calendar event dialog

--- client/src/routes/gym/schedule/+page.svelte
+++ client/src/routes/gym/schedule/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import * as Dialog from '$lib/components/ui/dialog';
+
 	let schedule = {
 		mon: [
 			{ title: 'No-gi', start: '11:14', end: '12:30', colour: '#f456' },
@@ -15,6 +17,7 @@
 		sun: []
 	};
 
+	let eventDialogOpen = $state(false);
 	let eventTitle;
 	let eventStart;
 	let eventEnd;
@@ -32,46 +35,36 @@
 		return `${Math.floor(durationMinutes / 60)}h ${durationMinutes % 60}m`;
 	}
 
-	function eventModal(title, start, end, colour) {
+	function eventDialog(title, start, end, colour) {
 		eventTitle = title;
 		eventStart = start;
 		eventEnd = end;
 		eventColour = colour;
 
-		eventInfoModal.showModal();
+		eventDialogOpen = true;
 	}
 </script>
 
-<dialog class="modal" id="eventInfoModal">
-	<div class="modal-box">
-		<form method="dialog">
-			<button class="btn btn-sm btn-circle btn-ghost absolute top-2 right-2">✕</button>
-		</form>
-		<h3 class="text-lg font-bold">{eventTitle}</h3>
-		<p class="py-4">{eventStart} - {eventEnd}</p>
-	</div>
-</dialog>
-
 <div class="grid grid-cols-8 p-4 text-sm">
 	<div class="col-span-1 border-b-2 border-gray-300 font-bold">Time</div>
-	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Monday</div>
-	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Tuesday</div>
-	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Wednesday</div>
-	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Thursday</div>
-	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Friday</div>
-	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Saturday</div>
-	<div class="col-span-1 border-b-1 border-gray-300 text-center font-bold">Sunday</div>
+	<div class="border-b-1 col-span-1 border-gray-300 text-center font-bold">Monday</div>
+	<div class="border-b-1 col-span-1 border-gray-300 text-center font-bold">Tuesday</div>
+	<div class="border-b-1 col-span-1 border-gray-300 text-center font-bold">Wednesday</div>
+	<div class="border-b-1 col-span-1 border-gray-300 text-center font-bold">Thursday</div>
+	<div class="border-b-1 col-span-1 border-gray-300 text-center font-bold">Friday</div>
+	<div class="border-b-1 col-span-1 border-gray-300 text-center font-bold">Saturday</div>
+	<div class="border-b-1 col-span-1 border-gray-300 text-center font-bold">Sunday</div>
 
 	{#each Array.from({ length: 24 }, (_, i) => i) as hour}
 		<div class="col-span-1 font-bold">{formatTime(`${hour}:00`)}</div>
 		{#each Object.keys(schedule) as day}
 			{#if schedule[day].some((event) => event.start.startsWith(`${hour}:`) || event.end.startsWith(`${hour}:`))}
-				<div class="col-span-1 border-1 border-blue-300">
+				<div class="border-1 col-span-1 border-blue-300">
 					{#each schedule[day] as event}
 						{#if event.start.split(':')[0] === `${hour}` || event.end.split(':')[0] === `${hour}`}
 							{#if event.start.split(':')[0] === `${hour}`}
 								<button
-									on:click={() => eventModal(event.title, event.start, event.end)}
+									onclick={() => eventDialog(event.title, event.start, event.end)}
 									class="w-full bg-blue-200"
 								>
 									<div class="font-bold">{event.title}</div>
@@ -80,7 +73,7 @@
 							{/if}
 							{#if event.end.split(':')[0] === `${hour}`}
 								<button
-									on:click={() => eventModal(event.title, event.start, event.end)}
+									onclick={() => eventDialog(event.title, event.start, event.end)}
 									class="size-full min-h-8 bg-blue-200"
 									aria-label="end-time-block"
 								></button>
@@ -89,8 +82,20 @@
 					{/each}
 				</div>
 			{:else}
-				<div class="col-span-1 border-1 border-gray-300"></div>
+				<div class="border-1 col-span-1 border-gray-300"></div>
 			{/if}
 		{/each}
 	{/each}
 </div>
+
+<Dialog.Root bind:open={eventDialogOpen}>
+	<Dialog.Trigger />
+	<Dialog.Content>
+		<Dialog.Header>
+			<Dialog.Title>{eventTitle}</Dialog.Title>
+			<Dialog.Description>
+				{eventStart} - {eventEnd}
+			</Dialog.Description>
+		</Dialog.Header>
+	</Dialog.Content>
+</Dialog.Root>
