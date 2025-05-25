5054beed53728fc939ea99ba94615009dc24e108
Luca <auth@luca.cx>
2025-05-25 18:31:24 +0200

    Change arg type

--- server/app/services.py
+++ server/app/services.py
@@ -1,17 +1,17 @@
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 
 
-def get_current_week(date: datetime) -> dict:
+def get_week(date: date) -> dict:
     # Round down to the nearest Monday
     days_to_subtract = date.weekday()
     if days_to_subtract != 0:  # If it's not already Monday
         date -= timedelta(days=days_to_subtract)
- 
+
     # Build the dictionary
-    days_of_week = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+    days_of_week = ("mon", "tue", "wed", "thu", "fri", "sat", "sun")
     week_current = {}
-    for i, day in enumerate(days_of_week):
-        week_current[day] = date + timedelta(days=i)
+    for item, day in enumerate(days_of_week):
+        week_current[day] = date + timedelta(days=item)
 
     return week_current
 
